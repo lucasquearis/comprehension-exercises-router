82f3ef56883bf7f7aadc21b646453e1bf2564d0f
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-07-11 15:06:01 -0300

    feito ate ex 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Route } from 'react-router-dom'
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/users" component={Users} />
+      </BrowserRouter>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <div>
+          <Link to="/about">About</Link>
+        </div>
+        <div>
+          <Link to="/users">Users</Link>
+        </div>
         <h2>Home</h2>
         <p> My awesome Home component </p>
       </div>
